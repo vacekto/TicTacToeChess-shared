d8736d258ea2d6c5128161b35bceac955c676394
Tomas Vacek <vacekto@gmail.com>
2023-02-07 08:44:48 +0100

    added initilize UltimateTicTacToe

--- src/ticTacToe.ts
+++ src/ticTacToe.ts
@@ -1,4 +1,4 @@
-import { TTicTacToeState } from './types'
+import { TTicTacToeState, TUltimateTicTacToeState } from './types'
 
 export const initializeTicTacToeState = (size: number): TTicTacToeState => {
     const state: TTicTacToeState = [];
@@ -6,6 +6,17 @@ export const initializeTicTacToeState = (size: number): TTicTacToeState => {
         state[i] = []
         for (let j = 0; j < size; j++) {
             state[i][j] = null
+        }
+    }
+    return state
+}
+
+export const initializeUltimateTicTacToeState = (): TUltimateTicTacToeState => {
+    const state: TUltimateTicTacToeState = [];
+    for (let i = 0; i < 3; i++) {
+        state[i] = []
+        for (let j = 0; j < 3; j++) {
+            state[i][j] = initializeTicTacToeState(3)
         }
     }
     return state
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
 export type TGameName = 'TicTacToe' | 'UltimateTicTacToe' | 'Chess'
 export type TTicTacToeState = ('X' | 'O' | null)[][]
+export type TUltimateTicTacToeState = (TTicTacToeState)[][]
 export type TTicTacToeSide = 'X' | 'O'
