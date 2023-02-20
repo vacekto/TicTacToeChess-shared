72f115ac9a11bae146c0af94542bf1f882352f50
Tomas Vacek <vacekto@gmail.com>
2023-02-20 12:07:55 +0100

    bugFix

--- src/gameLogic/chess.ts
+++ src/gameLogic/chess.ts
@@ -1,42 +1,30 @@
 import { IChessState, TChessBoard, TChessSide } from "../types"
 
-type TLinearDir = (COORD: [number, number], i: number) => [number, number]
-
-type TKnightDir = (COORD: [number, number]) => [number, number]
-
-type TDir = TLinearDir | TKnightDir
-
-type TInitChessState = (
-    topSidePlayer?: TChessSide,
-) => IChessState
-
+type TDir = (COORD: [number, number], i: number) => [number, number]
 interface IPieceDirs {
     [key: string]: TDir[],
 }
-
-const isOutOfBounds = ([X, Y]: [number, number]) => (X < 0 || Y < 0 || X > 7 || Y > 7)
-
-const diagonal: TLinearDir[] = [
+const diagonal: TDir[] = [
     ([X, Y], i) => [X + i, Y + i],
     ([X, Y], i) => [X + i, Y - i],
     ([X, Y], i) => [X - i, Y + i],
     ([X, Y], i) => [X - i, Y - i],
 ]
-const orthogonal: TLinearDir[] = [
+const orthogonal: TDir[] = [
     ([X, Y], i) => [X + i, Y],
     ([X, Y], i) => [X - i, Y],
     ([X, Y], i) => [X, Y + i],
     ([X, Y], i) => [X, Y - i],
 ]
-const knight: TKnightDir[] = [
-    ([X, Y]) => [X - 2, Y + 1],
-    ([X, Y]) => [X - 2, Y - 1],
-    ([X, Y]) => [X + 1, Y + 2],
-    ([X, Y]) => [X - 1, Y + 2],
-    ([X, Y]) => [X + 2, Y + 1],
-    ([X, Y]) => [X + 2, Y - 1],
-    ([X, Y]) => [X + 1, Y - 2],
-    ([X, Y]) => [X - 1, Y - 2],
+const knight: TDir[] = [
+    ([X, Y], i) => [X - 2 * i, Y + 1 * i],
+    ([X, Y], i) => [X - 2 * i, Y - 1 * i],
+    ([X, Y], i) => [X + 1 * i, Y + 2 * i],
+    ([X, Y], i) => [X - 1 * i, Y + 2 * i],
+    ([X, Y], i) => [X + 2 * i, Y + 1 * i],
+    ([X, Y], i) => [X + 2 * i, Y - 1 * i],
+    ([X, Y], i) => [X + 1 * i, Y - 2 * i],
+    ([X, Y], i) => [X - 1 * i, Y - 2 * i],
 ]
 
 const pieceDirs: IPieceDirs = {
@@ -52,7 +40,7 @@ export class ChessGame {
     private board: TChessBoard
     private topSidePlayer: TChessSide
     private activePlayer: TChessSide
-    private kings: {
+    private kingsPos: {
         w: [number, number]
         b: [number, number]
     }
@@ -64,18 +52,94 @@ export class ChessGame {
             board: this.board,
             topSidePlayer: this.topSidePlayer,
             activePlayer: this.activePlayer,
-            kings: this.kings,
+            kingsPos: this.kingsPos,
             winner: this.winner
         })
     }
 
 
     constructor(topSidePlayer: TChessSide = 'black') {
-        const [w, b] = topSidePlayer === 'black' ? [0, 7] : [7, 0]
+        const [w, b] = topSidePlayer === 'black' ? [7, 0] : [0, 7]
         this.board = initChessBoard(topSidePlayer)
         this.activePlayer = 'white'
         this.topSidePlayer = topSidePlayer
-        this.kings = { w: [w, 4], b: [b, 4] }
+        this.kingsPos = { w: [w, 4], b: [b, 4] }
+        this.winner = null
+    }
+
+    private getPotentialMoves([X, Y]: [number, number]) {
+        if (this.isOutOfBounds([X, Y])) return []
+        const piece = this.board[X][Y]
+
+        if (piece[1] === 'p') {
+            return this.getPawnMoves([X, Y])
+        }
+
+        let limit = 8
+        let moves: [number, number][] = []
+        if (piece[1] === 'n' || piece[1] === 'k') limit = 2
+        for (let dir of pieceDirs[piece[1]]) {
+            const movesInDir = this.getMovesInDir(dir, [X, Y], limit)
+            moves = [...moves, ...movesInDir]
+        }
+
+        return moves
+    }
+
+    getLegalMoves([X, Y]: [number, number]) {
+        if (this.isOutOfBounds([X, Y]) || this.board[X][Y] === 'ee') {
+            return []
+        }
+        let moves = this.getPotentialMoves([X, Y])
+        const piece = this.board[X][Y]
+        for (let [A, B] of moves) {
+            const save = this.board[A][B]
+            this.move([X, Y], [A, B], false)
+            if (this.isKingEndangered(piece[0] as 'w' | 'b')) {
+                moves = moves.filter(coord => {
+                    return (coord[0] !== A || B !== coord[1])
+                })
+            }
+            this.move([A, B], [X, Y], false)
+            this.board[A][B] = save
+        }
+
+        return moves
+    }
+
+    isKingEndangered(side: 'w' | 'b') {
+        const [A, B] = this.kingsPos[side]
+        let moves: [number, number][]
+        const enemy = side === 'w' ? 'b' : 'w'
+        for (const [X, row] of Object.entries(this.board)) {
+            for (const [Y, square] of Object.entries(row)) {
+                if (square[0] === enemy) {
+                    moves = this.getPotentialMoves([+X, +Y])
+                    for (let move of moves) {
+                        if (move[0] === A && move[1] === B) return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    move(moveFrom: [number, number], moveTo: [number, number], toggleActivePlayer: boolean = true) {
+        const [X, Y] = moveFrom
+        const [A, B] = moveTo
+        if (this.board[X][Y][0] !== this.activePlayer[0]) return
+        this.board[A][B] = this.board[X][Y]
+        this.board[X][Y] = 'ee'
+        if (toggleActivePlayer) {
+            this.toggleActivePlayer()
+        }
+    }
+
+    resetState() {
+        this.board = initChessBoard('black')
+        this.activePlayer = 'white'
+        this.topSidePlayer = 'black'
+        this.kingsPos = { w: [7, 4], b: [0, 4] }
         this.winner = null
     }
 
@@ -92,35 +156,25 @@ export class ChessGame {
         this.board = copy
     }
 
+    private isOutOfBounds([X, Y]: [number, number]) {
+        return (X < 0 || Y < 0 || X > 7 || Y > 7)
+    }
 
-    private isKingEndangered() {
-        const AP = this.activePlayer[0] as 'w' | 'b'
-        const [X, Y] = this.kings[AP]
-
+    private toggleActivePlayer() {
+        this.activePlayer = this.activePlayer === 'black' ? 'white' : 'black'
     }
 
     private getMovesInDir(moveInDir: TDir, [X, Y]: [number, number], limit: number) {
         const [piece, board] = [this.board[X][Y], this.board]
-        const moves = []
+        const moves: [number, number][] = []
 
         for (let i = 1; i < limit; i++) {
             const [A, B] = moveInDir([X, Y], i)
-            if (isOutOfBounds([A, B]) || board[A][B][0] === piece[0]) break
+            if (this.isOutOfBounds([A, B]) || board[A][B][0] === piece[0]) break
             moves.push([A, B])
             if (board[A][B] !== 'ee') break
         }
         return moves
-    }
-
-
-    private addMoveIfLegal([X, Y]: [number, number], moveList: [number, number,][]) {
-        if (isOutOfBounds([X, Y])) return
-        if (this.board[X][Y][0] === this.activePlayer[0]) return
-        moveList.push([X, Y])
-    }
-
-    private addMovesIfLegal(moves: [number, number][], moveList: [number, number][]) {
-        moves.forEach(move => this.addMoveIfLegal(move, moveList))
     }
 
 
@@ -135,91 +189,27 @@ export class ChessGame {
         if (piece[1] !== 'p') return []
 
         if (board[X + i][Y] === 'ee') {
-            this.addMoveIfLegal([X + i, Y], moves)
+            moves.push([X + i, Y])
         }
 
-        /* is shorter always??
-    
-        [Y + i, Y - i].forEach((column) => {
-            if (!isOutOfBounds([X + i, column])
-                && board[X + i][column][0] !== piece[0]
-                && board[X + i][column] !== 'ee'
-            ) this.addMoveIfLegal([X + i, column], moves)
-        })
-        */
-
-        if (!isOutOfBounds([X + i, Y + 1])
+        if (!this.isOutOfBounds([X + i, Y + 1])
             && board[X + i][Y + 1][0] !== piece[0]
             && board[X + i][Y + 1] !== 'ee'
-        ) this.addMoveIfLegal([X + i, Y + 1], moves)
+        ) moves.push([X + i, Y + 1])
 
-        if (!isOutOfBounds([X + i, Y - 1])
+        if (!this.isOutOfBounds([X + i, Y - 1])
             && board[X + i][Y - 1][0] !== piece[0]
             && board[X + i][Y - 1] !== 'ee'
-        ) this.addMoveIfLegal([X + i, Y - 1], moves)
+        ) moves.push([X + i, Y - 1])
 
 
         if ([1, 6].includes(X)
             && board[X + i][Y] === 'ee'
             && board[X + i + i][Y] === 'ee'
-        ) this.addMoveIfLegal([X + i + i, Y], moves)
+        ) moves.push([X + i + i, Y])
 
 
         return moves
-    }
-
-
-    /*private getKnightMoves([X, Y]: [number, number]) {
-        let moves: [number, number][] = []
-        if (isOutOfBounds([X, Y])) moves
-        const piece = this.board[X][Y]
-        if (piece[1] !== 'n') moves
-
-        for (let dir of pieceDirs[piece[1]]) {
-            const movesInDir = this.getMovesInDir(dir, [X, Y]) as [number, number][]
-            moves = [...moves, ...movesInDir]
-        }
-        return moves
-    }*/
-
-    private getKingMoves() {
-
-    }
-
-    getPotentialMoves([X, Y]: [number, number]) {
-        if (isOutOfBounds([X, Y])) return []
-        const piece = this.board[X][Y]
-
-        if (piece[1] === 'p') {
-            const moves = structuredClone(this.getPawnMoves([X, Y]))
-            return moves as [number, number][]
-        }
-
-        let limit = 8
-        if (piece[1] === 'n' || piece[1] === 'k') limit = 2
-        let moves: number[][] = []
-        for (let dir of pieceDirs[piece[1]]) {
-            const movesInDir = this.getMovesInDir(dir, [X, Y], limit)
-            moves = [...moves, ...movesInDir]
-        }
-
-        return structuredClone(moves) as [number, number][]
-    }
-
-    move([X, Y]: [number, number], [A, B]: [number, number]) {
-        const originPiece = this.board[X][Y]
-        if (originPiece[0] !== this.activePlayer[0]) return
-        this.board[A][B] = this.board[X][Y]
-        this.board[X][Y] = 'ee'
-        this.activePlayer = this.activePlayer === 'black' ? 'white' : 'black'
-    }
-
-    resetState() {
-        this.board = initChessBoard('black')
-        this.activePlayer = 'white'
-        this.topSidePlayer = 'black'
-        this.kings = { w: [7, 4], b: [0, 4] }
-        this.winner = null
     }
 
 }
